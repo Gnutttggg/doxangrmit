1989fc82b3f9ae9182f34e750d2979746b67ea09
Gnutttggg <133179243+Gnutttggg@users.noreply.github.com>
2023-08-04 00:57:20 -0400

    Update landingfirebase.js

--- landingfirebase.js
+++ landingfirebase.js
@@ -1,6 +1,6 @@
  // Import the functions you need from the SDKs you need
- import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
- import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-analytics.js";
+
+
  // TODO: Add SDKs for Firebase products that you want to use
  // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -10,12 +10,56 @@
    apiKey: "AIzaSyCqpme0ulRw4eHc6LSW64GKlXhkPKKuRH4",
    authDomain: "notii-8e38a.firebaseapp.com",
    projectId: "notii-8e38a",
+   databaseURL: "https://notii-8e38a-default-rtdb.asia-southeast1.firebasedatabase.app",
    storageBucket: "notii-8e38a.appspot.com",
    messagingSenderId: "909617956814",
    appId: "1:909617956814:web:f9c4440ee70f907b8d0d9f",
    measurementId: "G-B61BLH0DMK"
  };
 
- // Initialize Firebase
- const app = initializeApp(firebaseConfig);
- const analytics = getAnalytics(app);
+
+    
+ if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+  }
+    const database = firebase.database();
+  const storage = firebase.storage();
+  const storageRef = storage.ref();
+  
+  
+  
+
+  
+  function saveUserData(phoneNumber, latitude, longitude) {
+    const userDataRef = database.ref('user_data').push();
+    return userDataRef.set({
+        phoneNumber: phoneNumber,
+        location: {
+            latitude: latitude,
+            longitude: longitude
+        }
+    })
+    .then(() => {
+        console.log("User data saved successfully.");
+    })
+    .catch((error) => {
+        console.error("Error saving user data:", error);
+    });
+}
+
+function saveUserNumber(phoneNumber) {
+    const userDataRef = database.ref('user_number').push();
+    return userDataRef.set({
+        phoneNumber: phoneNumber
+        
+    })
+    .then(() => {
+        console.log("User data saved successfully.");
+    })
+    .catch((error) => {
+        console.error("Error saving user data:", error);
+    });
+}
+
+
+  
